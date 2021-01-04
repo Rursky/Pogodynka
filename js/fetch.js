8312be73af55abb855883383934fb055078e9217
Rursky <71942106+Rursky@users.noreply.github.com>
2021-01-04 17:22:06 +0100

    change currency on town

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,7 @@
-const currency = document.getElementById("currency")
+const town = document.getElementById("town")
 
-currency.addEventListener("change", function () {
-    document.getElementById("temp").innerText = `Prognoza 5 dniowa dla miasta: ${currency.value}`
+town.addEventListener("change", function () {
+    document.getElementById("temp").innerText = `Prognoza 5 dniowa dla miasta: ${town.value}`
 })
 
 const DOM_img = document.createElement("img");
@@ -20,8 +20,8 @@ for(let i=0; i<images_temperature.length; i++){ // pętla tworząca "puste" img 
 const outputs = ["output", "output1", "output2", "output3", "output4"]
 const nums = [0, 8, 16, 24, 32]
 
-currency.addEventListener("change", function () {
-    fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${currency.value}&appid=3865d70b9f135bc376b8beb376bb474b`)
+town.addEventListener("change", function () {
+    fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${town.value}&appid=3865d70b9f135bc376b8beb376bb474b`)
         .then( resp => resp.json())
         .then( data => {
             for(let j=0; j<outputs.length; j++){

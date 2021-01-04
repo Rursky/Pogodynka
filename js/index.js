fdbde71a1a26dce5e55f98eb42dec941a38ecb20
Rursky <71942106+Rursky@users.noreply.github.com>
2021-01-04 13:00:45 +0100

    Delete index.js

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const currency = document.getElementById("currency")
-
-currency.addEventListener("change", function () {
-    document.getElementById("temp").innerText = `Prognoza 5 dniowa dla miasta: ${currency.value}`
-})

0ac57ad4bd974a8a3092737dbc698db8b6c39c07
Rursky <71942106+Rursky@users.noreply.github.com>
2021-01-05 12:20:11 +0100

    v2 - tables creates by loops

--- js/fetch.js
+++ js/fetch.js
@@ -1,24 +1,25 @@
 const town = document.getElementById("town")
 
-town.addEventListener("change", function () {
+town.addEventListener("change", function () { // nasluchiwanie na "zmiany" selecta w celu pokazaniu napisu "Prognoza 5[...]"
     document.getElementById("temp").innerText = `Prognoza 5 dniowa dla miasta: ${town.value}`
 })
 
-const DOM_img = document.createElement("img");
-const DOM_img1 = document.createElement("img");
-const DOM_img2 = document.createElement("img");
-const DOM_img3 = document.createElement("img");
-const DOM_img4 = document.createElement("img");
-
-const images_temperature = [DOM_img, DOM_img1, DOM_img2, DOM_img3, DOM_img4];
-const images_html = ["image", "image1", "image2", "image3", "image4"];
-
-for(let i=0; i<images_temperature.length; i++){ // pętla tworząca "puste" img bez src
-    document.getElementById(images_html[i]).appendChild(images_temperature[i]);
+const html_elements_number = 5 // wpisz ile masz "okienek" dla pogody - w tym wypadku 5 bo prognoza na 5 dni / musisz ręcznie dopisać okienka w html
+// <div class="windows"><div id="outputX"></div><div id="imageX"></div></div>
+const images_html = []
+const outputs = []
+const nums = []
+for(let i=0; i<html_elements_number; i++){
+    images_html.push(`image${i}`)
+    outputs.push(`output${i}`)
+    nums.push(i*8)
 }
 
-const outputs = ["output", "output1", "output2", "output3", "output4"]
-const nums = [0, 8, 16, 24, 32]
+const pictures = []
+for(let i=0; i<images_html.length; i++){
+    document.getElementById(images_html[i]).innerHTML = `<img id=img${i} src="">`;
+    pictures.push(document.getElementById(`img${i}`))
+}
 
 town.addEventListener("change", function () {
     fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${town.value}&appid=3865d70b9f135bc376b8beb376bb474b`)
@@ -28,21 +29,23 @@ town.addEventListener("change", function () {
                 document.getElementById(outputs[j]).innerText = "Dzień: " + data.list[nums[j]].dt_txt + ", temperatura: " + ((data.list[nums[j]].main.temp)-273.15).toFixed(2) + "°c";
                 switch(true){
                     case (((data.list[nums[j]].main.temp)-273.15) < 0):
-                        images_temperature[j].src = "https://svgshare.com/getbyhash/sha1-pgVZNzECL/moe+8E8C6zGpLHHLs="
+                        pictures[j].src = "https://svgshare.com/getbyhash/sha1-pgVZNzECL/moe+8E8C6zGpLHHLs="
                         break;
                     case ( (((data.list[nums[j]].main.temp)-273.15) >= 0) && (((data.list[nums[j]].main.temp)-273.15) < 5) ):
-                        images_temperature[j].src = "https://svgshare.com/getbyhash/sha1-erTcmRD/+pygI17D0Laqe09jlYU="
+                        pictures[j].src = "https://svgshare.com/getbyhash/sha1-erTcmRD/+pygI17D0Laqe09jlYU="
                         break;
                     case ( (((data.list[nums[j]].main.temp)-273.15) >= 5) && (((data.list[nums[j]].main.temp)-273.15) < 15) ):
-                        images_temperature[j].src = "https://svgshare.com/getbyhash/sha1-vmHVc9B7LqtQstwyH4Rnyc9lPIs="
+                        pictures[j].src = "https://svgshare.com/getbyhash/sha1-vmHVc9B7LqtQstwyH4Rnyc9lPIs="
                         break;
                     case ( (((data.list[nums[j]].main.temp)-273.15) >= 15) && (((data.list[nums[j]].main.temp)-273.15) < 27) ):
-                        images_temperature[j].src = "https://svgshare.com/getbyhash/sha1-p/plJ4g+zdMEcfsgD0/CNL05vLg="
+                        pictures[j].src = "https://svgshare.com/getbyhash/sha1-p/plJ4g+zdMEcfsgD0/CNL05vLg="
                         break;
                     case (((data.list[nums[j]].main.temp)-273.15) >= 27):
-                        images_temperature[j].src = "https://svgshare.com/getbyhash/sha1-JWWlgjTlz97B1sZguAvp0cNgtWM="
+                        pictures[j].src = "https://svgshare.com/getbyhash/sha1-JWWlgjTlz97B1sZguAvp0cNgtWM="
                         break;
+
                 }
+
             }
         })
 })
